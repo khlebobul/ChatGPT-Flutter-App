0d5d020b12237ce74fda17bfbd9f1b6bd425bf9d
gleb <sbgleb10@gmail.com>
2023-03-15 15:56:23 +0300

    build a chat screen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter/src/widgets/container.dart';
-// import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 import '../services/assets_manager.dart';
 
@@ -12,10 +11,90 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final bool _isTyping = true;
+
+  late TextEditingController textEditingController;
+
+  @override
+  void initState() {
+    textEditingController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Image.asset(AssetsManager.openaiLogo)),
+      appBar: AppBar(
+        elevation: 2,
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset(AssetsManager.openaiLogo),
+        ),
+        title: const Text('Chat GPT'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.more_vert_rounded,
+              color: Colors.white,
+            ),
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Flexible(
+              child: ListView.builder(
+                itemCount: 6,
+                itemBuilder: (context, index) {
+                  return const Text("Hello!");
+                },
+              ),
+            ),
+            if (_isTyping) ...[
+              const SpinKitThreeBounce(
+                color: Colors.grey,
+                size: 18,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Material(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          style: const TextStyle(color: Colors.grey),
+                          controller: textEditingController,
+                          onSubmitted: (value) {
+                            // Todo send message
+                          },
+                          decoration: const InputDecoration.collapsed(
+                              hintText: 'How can I help you',
+                              hintStyle: TextStyle(color: Colors.grey)),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.send, color: Colors.grey),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ]
+          ],
+        ),
+      ),
     );
   }
 }
