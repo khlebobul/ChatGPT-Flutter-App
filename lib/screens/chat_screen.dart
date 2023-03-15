c5bc74108e6e1a0ed7c1641b08bd9eb0dfaefdcb
gleb <sbgleb10@gmail.com>
2023-03-15 12:00:00 +0300

    setting the app

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+// import 'package:flutter/src/widgets/container.dart';
+// import 'package:flutter/src/widgets/framework.dart';
 
 import '../services/assets_manager.dart';
 
