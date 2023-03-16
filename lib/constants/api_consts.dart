30db8f36ce598b84e003bf3211b85f0dd8806bd1
gleb <sbgleb10@gmail.com>
2023-03-16 17:30:55 +0300

    managing api and json

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,2 +1,2 @@
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "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";
